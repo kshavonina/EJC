9044f1bf130a4d2ec6e2bb45cede3f41cbc25de4
kshavonina <kshavonina@yandex.ru>
2017-07-06 18:22:38 +0300

    I've started 2

--- src/main/java/task03/Main.java
+++ src/main/java/task03/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         // fjeglgkrs
     }
+
+    // create generateMap for creating battle field
+
+    // create playing process (method, I think void one)
 }
