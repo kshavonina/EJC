a9057bc8078b3f26b0ef44796b776ffb24cbdcc0
kshavonina <kshavonina@yandex.ru>
2017-07-29 20:02:24 +0300

    add javadoc to task_01

--- src/main/java/task_01/HelloWorld.java
+++ src/main/java/task_01/HelloWorld.java
@@ -1,5 +1,11 @@
 package task_01;
 
+/**
+ * Our first home task: print "Hello World!" to the console.
+ *
+ * @author Kseniya Shavonina
+ * @version 1.0
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
