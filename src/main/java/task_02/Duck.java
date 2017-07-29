63e139944104fb38c1f41b912b9f9a6ea7d73042
kshavonina <kshavonina@yandex.ru>
2017-07-29 20:22:36 +0300

    added javadoc to class Duck

--- src/main/java/task_02/Duck.java
+++ src/main/java/task_02/Duck.java
@@ -1,8 +1,20 @@
 package task_02;
 
+/**
+ * The class Duck represents a duck
+ * with its own fly behaviour.
+ *
+ * @author Kseniya Shavonina
+ * @version 1.0
+ */
 public class Duck {
+    /** Represents a type of fly behaviour. */
     FlyBehavior flyBehavior;
 
+    /**
+     * Performs a flight in accordance with
+     * the type of duck's fly behaviour.
+     */
     public void performFly() {
         flyBehavior.fly();
     }
