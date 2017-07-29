37de4c0f02522c2316354758861a79aca1e8637a
kshavonina <kshavonina@yandex.ru>
2017-07-29 20:36:54 +0300

    added javadocs to CommonDuck and WoodenDuck classes

--- src/main/java/task_02/CommonDuck.java
+++ src/main/java/task_02/CommonDuck.java
@@ -1,6 +1,16 @@
 package task_02;
 
+/**
+ * The CommonDuck class represents a common duck.
+ *
+ * @author Kseniya Shavonina
+ * @version 1.0
+ */
 public class CommonDuck extends Duck {
+    /**
+     * Initializes a newly created object
+     * with FlyWithWings fly behaviour.
+     */
     public CommonDuck() {
         flyBehavior = new FlyWithWings();
     }
--- src/main/java/task_02/WoodenDuck.java
+++ src/main/java/task_02/WoodenDuck.java
@@ -1,6 +1,17 @@
 package task_02;
 
+/**
+ * The WoodenDuck class represents a wooden duck.
+ *
+ * @author Kseniya Shavonina
+ * @version 1.0
+ */
 public class WoodenDuck extends Duck {
+    /**
+     * Initializes a newly created object
+     * with FlyNoWay fly behaviour, since
+     * wooden duck can not fly.
+     */
     public WoodenDuck() {
         flyBehavior = new FlyNoWay();
     }
