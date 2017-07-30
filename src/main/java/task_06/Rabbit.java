1f1a4b5d6e5da30355b411623baeb1dadc392915
kshavonina <kshavonina@yandex.ru>
2017-07-30 23:39:32 +0300

    updated example with a rabbit that eats carrot (for better understanding of "volatile"). also created SausageShop class.

--- src/main/java/task_06/Rabbit.java
+++ src/main/java/task_06/Rabbit.java
@@ -2,7 +2,19 @@ package task_06;
 
 import java.io.IOException;
 
+/**
+ * Multithreading. Example of using key word "volatile".
+ * Rabbit eats carrot until we press "Enter".
+ *
+ * @author Kseniya Shavonina
+ * @version 1.0
+ */
 public class Rabbit {
+    /**
+     * The flag indicates whether the rabbit is eating
+     * carrot or not. If it doesn't have "volatile" modifier,
+     * threads will not have the last value of it.
+     */
     public volatile static boolean isEating = false;
 
     public static void main(String[] args) {
@@ -10,14 +22,26 @@ public class Rabbit {
         new Rabbit.StopEating().start();
     }
 
+    /**
+     * The EatCarrot class represents rabbit eating carrot.
+     */
     public static class EatCarrot extends Thread {
         @Override
         public void run() {
             isEating = true;
             System.err.println("Rabbit is eating.");
+
+            while (isEating) {
+
+            }
+
+            System.err.println("Rabbit stopped eating.");
         }
     }
 
+    /**
+     * The StopEating class represents stop for eating carrot.
+     */
     public static class StopEating extends Thread {
         @Override
         public void run() {
@@ -28,7 +52,7 @@ public class Rabbit {
             }
 
             isEating = false;
-            System.err.println("Rabbit stopped eating.");
+            System.err.println("And we finally say: rabbit, enough to eat!");
         }
     }
 }
---
+++ src/main/java/task_06/SausageShop.java
@@ -0,0 +1,4 @@
+package task_06;
+
+public class SausageShop {
+}
