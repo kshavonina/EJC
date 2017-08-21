75f7b787b9929d6385fc80f2459c706b6623d7bd
kshavonina <kshavonina@yandex.ru>
2017-08-21 14:04:39 +0300

    javadocs added and report included

--- src/main/java/task_11/Main.java
+++ src/main/java/task_11/Main.java
@@ -1,5 +1,11 @@
 package task_11;
 
+/**
+ * Entry point for csv parser.
+ *
+ * @author Kseniya Shavonina
+ * @version 1.0
+ */
 public class Main {
     public static void main(String[] args) {
         CSVParser parser = new CSVParser();
