ed7a3834534020d93816817402cdeb31b83a43e2
kshavonina <kshavonina@yandex.ru>
2017-08-26 00:59:53 +0300

    first example of recursion

--- src/main/java/tasks/Recursion.java
+++ src/main/java/tasks/Recursion.java
@@ -2,6 +2,18 @@ package tasks;
 
 public class Recursion {
     public static void main(String[] args) {
+        Recursion recursion = new Recursion();
+        System.out.println(recursion.factorial(5));
+    }
 
+    public int factorial(int n) {
+        int result;
+
+        if (n == 1) {
+            return 1;
+        }
+
+        result = factorial(n - 1) * n;
+        return result;
     }
 }
